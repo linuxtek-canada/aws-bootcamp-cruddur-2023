ef5aa23f647835b2860b1ca269ff1f5cbf755009
linuxtek-canada <jasonpa@gmail.com>
2023-02-28 00:07:23 +0000

    Adding Honeycomb Instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,25 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Honeycomb - Imports
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Honeycomb - Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+# Honeycomb - Initialize automatic instrumentation with Flask
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
--- journal/week2.md
+++ journal/week2.md
@@ -1,1 +1,9 @@
 # Week 2 — Distributed Tracing
+
+## Required Homework
+
+### Video Review
+
+### Actions
+
+* Completed all steps during the livestream to set up Honeycomb distributed tracing.
